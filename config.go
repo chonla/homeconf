c3fb8956aa5ff977cb85cbf5bae44c3cfd815bf2
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-11-06 22:45:25 +0700

    Ensure file existence once initialized

--- config.go
+++ config.go
@@ -45,6 +45,7 @@ func ensure(filename string) error {
 
 	confFileName := fmt.Sprintf(`%s%s%s`, confPath, string(os.PathSeparator), filename)
 
+	log.Printf("ensuring %s\n", confFileName)
 	fd, e := os.OpenFile(confFileName, os.O_RDONLY|os.O_CREATE, 0666)
 	if e != nil {
 		return e
